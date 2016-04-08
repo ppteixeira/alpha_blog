7a9ff36a604d6fae74faf0ba4f4f9157584bfff7
PEDRO ARAUJO <ppteixeira@outlook.com>
2016-04-08 16:35:14 +1000

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
